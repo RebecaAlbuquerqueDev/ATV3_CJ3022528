45adfc4c434ec61d7d76880114450c9d33998843
RebecaAlbuquerqueDev <rebeca.albuquerque@aluno.ifsp.edu.br>
2024-03-06 10:15:49 -0300

    Adicionar arquivos de projeto.

---
+++ ATV3_CJ3022528/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace ATV3_CJ3022528
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite sua nota");
+            double nota =
+                Convert.ToDouble(Console.ReadLine());
+
+            //1//
+
+            if (nota > +6)
+            {
+                Console.WriteLine("O aluno passou");
+            }
+            else
+            {
+                Console.WriteLine("O aluno reprovou");
+
+                
+
+
+                Console.WriteLine("Digite qualquer  numero");
+
+                //2//
+                Console.WriteLine("Por favor, insira um número:");
+                int numero = Convert.ToInt32(Console.ReadLine());
+
+                
+                if (numero % 2 == 0)
+                {
+                    Console.WriteLine($"O número {numero} é par.");
+                }
+                else
+                {
+                    Console.WriteLine($"O número {numero} é ímpar.");
+                }
+
+            }
+        }
+    }
+}
+
+            
+        
+   
